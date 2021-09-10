c01e7267f417f2a339534889bfa48467e4d09456
dk4g <dkcomc@gmail.com>
2021-09-10 07:36:58 +0530

    Initial Commit

--- src/main/java/com/dk/SpringMVC/controllers/WebController.java
+++ src/main/java/com/dk/SpringMVC/controllers/WebController.java
@@ -1,15 +1,26 @@
 package com.dk.SpringMVC.controllers;
 
 
+import com.dk.SpringMVC.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class WebController {
 
     @GetMapping("/list")
-    public String getList() {
+    public String getList(Model model) {
 
+        List<User> userList = new ArrayList<>();
+        userList.add(new User("Robert", "Smith", "New York"));
+        userList.add(new User("Stephen", "Collins", "Washington DC"));
+
+        model.addAttribute("userList", userList);
         return "list";
     }
 }
--- src/main/webapp/WEB-INF/jsp/list.jsp
+++ src/main/webapp/WEB-INF/jsp/list.jsp
@@ -1,10 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>List</title>
 </head>
-<body>
 <h2> User List</h2>
+<body>
+    <table>
+        <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>City</th>
+
+        </tr>
+        <c:forEach var="user" items="${userList}">
+            <tr>
+               <td> <c:out value="${user.firstName}" /> </td>
+
+               <td> <c:out value="${user.lastName}" /></td>
+
+               <td> <c:out value="${user.city}" /></td>
+            </tr>
+        </c:forEach>
+    </table>
 </body>
 </html>
